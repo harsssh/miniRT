f52e1a976052598d6624bbca42febed58c07d096
harsh <66548698+harsssh@users.noreply.github.com>
2023-12-15 22:29:30 +0900

    fix equal

--- src/vector/comparison.c
+++ src/vector/comparison.c
@@ -6,16 +6,19 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 12:20:17 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/12/15 12:20:19 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/12/15 22:29:01 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vector.h"
 
+#define EPSILON 0.000000001
+
 // equality
 bool	vec3_equal(t_vec3 a, t_vec3 b)
 {
-	return (a.x == b.x && a.y == b.y && a.z == b.z);
+	return (fabs(a.x - b.x) < EPSILON && fabs(a.y - b.y) < EPSILON
+		&& fabs(a.z - b.z) < EPSILON);
 }
 
 // inequality
