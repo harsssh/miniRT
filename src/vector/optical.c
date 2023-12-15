56dd1cff9fcb7671c66ae87a72f472a15197bdbe
harsh <66548698+harsssh@users.noreply.github.com>
2023-12-15 17:17:14 +0900

    tweak

--- src/vector/optical.c
+++ src/vector/optical.c
@@ -6,7 +6,7 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 16:50:44 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/12/15 16:57:58 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/12/15 17:16:57 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 // reflect
 // normal must be normalized
-t_vec3	vec3_reflect(t_vec3 incident, t_vec3 normal)
+t_vec3	vec3_reflect(t_vec3 v, t_vec3 normal)
 {
 	return (vec3_sub(
-			incident,
-			vec3_scale(normal, 2 * vec3_dot(incident, normal))));
+			v,
+			vec3_scale(normal, 2 * vec3_dot(v, normal))));
 }
 
-bool	vec3_is_total_reflection(t_vec3 incident, t_vec3 normal, double ref_idx)
+bool	vec3_is_total_reflection(t_vec3 v, t_vec3 normal, double ref_idx)
 {
 	double	cos_incident;
 	double	sin_refracted;
 
-	cos_incident = fmin(vec3_dot(vec3_negate(incident), normal), 1.0);
+	cos_incident = fmin(vec3_dot(vec3_negate(v), normal), 1.0);
 	sin_refracted = ref_idx * sqrt(
 			fmax(1.0 - cos_incident * cos_incident, 0.0));
 	return (sin_refracted > 1.0);
@@ -34,17 +34,17 @@ bool	vec3_is_total_reflection(t_vec3 incident, t_vec3 normal, double ref_idx)
 
 // refract
 // normal must be normalized
-t_vec3	vec3_refract(t_vec3 incident, t_vec3 normal, double ref_idx)
+t_vec3	vec3_refract(t_vec3 v, t_vec3 normal, double ref_idx)
 {
 	double	cos_incident;
 	t_vec3	out_parallel;
 	t_vec3	out_perp;
 
-	if (vec3_is_total_reflection(incident, normal, ref_idx))
+	if (vec3_is_total_reflection(v, normal, ref_idx))
 		return (vec3_zero());
-	cos_incident = fmin(vec3_dot(vec3_negate(incident), normal), 1.0);
+	cos_incident = fmin(vec3_dot(vec3_negate(v), normal), 1.0);
 	out_parallel = vec3_scale(vec3_add(
-				incident, vec3_scale(normal, cos_incident)), ref_idx);
+				v, vec3_scale(normal, cos_incident)), ref_idx);
 	out_perp = vec3_scale(normal,
 			-sqrt(fmax(1.0 - vec3_length_squared(out_parallel), 0.0)));
 	return (vec3_add(out_parallel, out_perp));
