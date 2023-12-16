0768cfcbbab066d88ecbfea65fd7786457e27026
harsh <66548698+harsssh@users.noreply.github.com>
2023-12-16 21:55:36 +0900

    restore main.c

--- src/main.c
+++ src/main.c
@@ -6,18 +6,13 @@
 /*   By: kemizuki <kemizuki@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 11:34:06 by kemizuki          #+#    #+#             */
-/*   Updated: 2023/12/16 21:37:58 by kemizuki         ###   ########.fr       */
+/*   Updated: 2023/12/15 11:36:23 by kemizuki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "debug.h"
+#include <stdio.h>
 
 int	main(void)
 {
-	t_parse_option	option;
-	t_config		*config;
-
-	option.max_light = 1;
-	config = parse_config("scenes/example.rt", option);
-	print_config(*config);
+	printf("miniRT\n");
 }
