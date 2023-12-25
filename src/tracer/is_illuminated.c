8c4bbe63fac99ba10191cf477fb03fabed75b5f9
harsh <66548698+harsssh@users.noreply.github.com>
2023-12-25 22:17:08 +0900

    is_illuminated

--- src/tracer/is_illuminated.c
+++ src/tracer/is_illuminated.c
@@ -14,8 +14,20 @@
 
 bool	is_illuminated(t_scene *scene, t_light_conf light, t_hit_record rec)
 {
-	(void)scene;
-	(void)light;
-	(void)rec;
-	return (false);
+	const t_ray shadow_ray = new_ray(rec.point,
+		vec3_normalize(vec3_sub(light.position, rec.point)));
+	t_hit_func hit_func;
+	t_hit_record shadow_rec;
+	t_node	*obj_node;
+
+	obj_node = scene->objects->head;
+	while (obj_node)
+	{
+		hit_func = get_hit_func(obj_node->data);
+		if (hit_func != NULL
+			&& hit_func(obj_node->data, shadow_ray, 1e-3, &shadow_rec))
+			return (false);
+		obj_node = obj_node->next;
+	}
+	return (true);
 }
