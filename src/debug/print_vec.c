3ff7df04ca2fb77251820f8a2e48efd80e1a72c6
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2023-12-26 01:32:26 +0900

    Fix debug function (#11)

--- src/debug/print_vec.c
+++ src/debug/print_vec.c
@@ -19,8 +19,10 @@ void	print_vec(t_vec3 vec, bool newline)
 		printf("\n");
 }
 
+// color is in range [0, 1]
 void	print_rgb(t_rgb color, bool newline)
 {
+	color = vec3_scale(color, 255);
 	printf("(%d, %d, %d)",
 		(int)rgb_get_r(color),
 		(int)rgb_get_g(color),
